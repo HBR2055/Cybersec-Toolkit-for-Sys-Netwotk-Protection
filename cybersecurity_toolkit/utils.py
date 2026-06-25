a646872bc32c1de3d2ed9d3ecdb5bab705e86bd3
HBR2055 <hbr2055@gmail.com>
2026-06-25 03:35:29 -0700

    Add files via upload

---
+++ cybersecurity_toolkit/utils.py
@@ -0,0 +1,10 @@
+def banner():
+    print("""
+====================================
+ CYBERSECURITY TOOLKIT
+====================================
+1. User Activity Monitoring
+2. Vulnerability Scan (Port Scanner)
+3. Password Security Analysis
+4. Exit
+""")
